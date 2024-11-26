5ec4ed206abcbc996055d9e5e5bef9ae189a120d
tabinasydaa <tabinasyda@gmail.com>
2024-11-26 23:15:53 +0700

    update intent untuk activity ke activity lain

--- app/src/main/java/com/example/hydrateyou/Profile.kt
+++ app/src/main/java/com/example/hydrateyou/Profile.kt
@@ -1,6 +1,9 @@
 package com.example.hydrateyou
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button // Gunakan tipe yang sesuai
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,33 @@ class Profile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
+
+        // Menghubungkan button ke variabel
+        val editBtn = findViewById<Button>(R.id.edit_btn) // Sesuaikan dengan tipe button yang benar
+        val kebijakanBtn = findViewById<ImageButton>(R.id.kebijakan_btn)
+        val keluarBtn = findViewById<ImageButton>(R.id.keluar_btn)
+
+        // Menambahkan klik listener untuk editBtn
+        editBtn.setOnClickListener {
+            // Intent untuk berpindah ke EditProfile Activity
+            val intent = Intent(this, EditProfile::class.java)
+            startActivity(intent)
+        }
+
+        // Menambahkan klik listener untuk kebijakanBtn
+        kebijakanBtn.setOnClickListener {
+            // Intent untuk berpindah ke KebijakanPrivasi Activity
+            val intent = Intent(this, KebijakanPrivasi::class.java)
+            startActivity(intent)
+        }
+
+        // Menambahkan klik listener untuk keluarBtn
+        keluarBtn.setOnClickListener {
+            // Intent untuk berpindah ke Login Activity
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
