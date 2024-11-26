4e711a3e4c058e63d3a066d7218e3cfdce3695ce
TabhitaTriana <bithhfp@gmail.com>
2024-11-26 22:28:02 +0700

    update intent

--- app/src/main/java/com/example/hydrateyou/PelacakAirActivity.kt
+++ app/src/main/java/com/example/hydrateyou/PelacakAirActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hydrateyou
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,36 @@ class PelacakAirActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val imageButton1 = findViewById<ImageButton>(R.id.ImageButton1)
+        val imageButton2 = findViewById<ImageButton>(R.id.Avatar1)
+        val imageButton3 = findViewById<ImageButton>(R.id.ImageButton3)
+        val imageButton4 = findViewById<ImageButton>(R.id.ImageButton4)
+        val imageButton5 = findViewById<ImageButton>(R.id.ImageButton5)
+        val imageButton6 = findViewById<ImageButton>(R.id.ImageButton6)
+
+        imageButton1.setOnClickListener {
+            sendWaterAmount(25)
+        }
+        imageButton2.setOnClickListener {
+            sendWaterAmount(50)
+        }
+        imageButton3.setOnClickListener {
+            sendWaterAmount(100)
+        }
+        imageButton4.setOnClickListener {
+            sendWaterAmount(200)
+        }
+        imageButton5.setOnClickListener {
+            sendWaterAmount(300)
+        }
+        imageButton6.setOnClickListener {
+            sendWaterAmount(400)
+        }
+
+    }
+    private fun sendWaterAmount(amount: Int) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("EXTRA_WATER_AMOUNT", amount)
+        startActivity(intent)
     }
 }
